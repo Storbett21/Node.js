3cf6ab212850b143b094c687cf31689dfbdbb635
Simon <simontorbett@gmail.com>
2024-07-11 20:44:40 -0400

    app de buscar directorios

--- ls-avanzado.js
+++ ls-avanzado.js
@@ -1,17 +1,39 @@
-const fs = require('node:fs/promises');
+const fs = require('node:fs/promises')
+const path = require('node:path')
 
+const folder = process.argv[2] ?? '.'
 
-console.log ('leyendo el primer archivo...')
-fs.readFile('./archivo.txt', 'utf-8')
-.then(text =>{
-    console.log ('primer texto:', text)
-})
+async function ls (folder) {
+  let files
+  try {
+    files = await fs.readdir(folder)
+  } catch {
+    console.error(`❌ No se pudo leer el directorio ${folder}`)
+    process.exit(1)
+  }
 
-console.log ('---> Hacer cosas mientras lee el archivo')
+  const filesPromises = files.map(async file => {
+    const filePath = path.join(folder, file)
+    let stats
 
-console.log ('leyendo el segundo archivo...')
-fs.readFile('./segundo-archivo.txt', 'utf-8')
-    .then(text => {
-        console.log ('segundo texto', text);
-    })
+    try {
+      stats = await fs.stat(filePath) // status - información del archivo
+    } catch {
+      console.error(`No se pudo leer el archivo ${filePath}`)
+      process.exit(1)
+    }
 
+    const isDirectory = stats.isDirectory()
+    const fileType = isDirectory ? 'd' : 'f'
+    const fileSize = stats.size.toString()
+    const fileModified = stats.mtime.toLocaleString()
+
+    return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`
+  })
+
+  const filesInfo = await Promise.all(filesPromises)
+
+  filesInfo.forEach(fileInfo => console.log(fileInfo))
+}
+
+ls(folder)
--- ls.js
+++ ls.js
@@ -1,14 +1,30 @@
-const fs = require('node:fs');
+// const fs = require('node:fs');
 
-fs.readdir('.',(err, files)=> { 
-    if(err) {
-        console.error('Error al leer el directorio: ', err)
-        return;
-    }
+// fs.readdir('.',(err, files)=> { 
+//     if(err) {
+//         console.error('Error al leer el directorio: ', err)
+//         return;
+//     }
 
-    files.forEach(file => {
+//     files.forEach(file => {
+//         console.log(file)
+//     })
+
+// })
+
+const fs = require('node:fs'/promises);
+
+fs.readdir('.')
+.then(files =>{ 
+files.forEach(file => {
         console.log(file)
     })
 
 })
 
+.catch (err => {
+    if (err) {
+    console.error('Error al leer el directorio: ', err)
+    return;
+}
+})
