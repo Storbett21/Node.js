2ff32d334c7bf7d44e2039ae3bf70ca2c3da5f55
Simon <simontorbett@gmail.com>
2024-07-13 16:01:48 -0400

    Instalar dependecnia de produccion

--- ls-avanzado.js
+++ ls-avanzado.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
-const picocolors = require('picocolors')  
+const pc = require('picocolors')  // Dependencia de produccion
 
 const folder = process.argv[2] ?? '.'
 
@@ -9,7 +9,7 @@ async function ls (folder) {
   try {
     files = await fs.readdir(folder)
   } catch {
-    console.error(`❌ No se pudo leer el directorio ${folder}`)
+    console.error(pc.red(`❌ No se pudo leer el directorio ${folder}`))
     process.exit(1)
   }
 
@@ -29,7 +29,7 @@ async function ls (folder) {
     const fileSize = stats.size.toString()
     const fileModified = stats.mtime.toLocaleString()
 
-    return `${fileType} ${file.padEnd(20)} ${fileSize.toString().padStart(10)} ${fileModified}`
+    return `${pc.bgMagenta(fileType)} ${pc.blue(file.padEnd(20))} ${pc.green(fileSize.toString().padStart(10))} ${pc.yellow(fileModified)}`
   })
 
   const filesInfo = await Promise.all(filesPromises)
