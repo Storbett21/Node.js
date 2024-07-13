85b5a157580e24d3cd9e8bf1b078cb2e0372db74
Simon <simontorbett@gmail.com>
2024-07-13 16:20:16 -0400

    instlar dependencias de devs

--- 1.os-info.js
+++ 1.os-info.js
@@ -8,6 +8,6 @@ console.log('nombre del sistema operativo', os.platform());
 console.log('version del sistema operativo', os.release());
 console.log('Arquitectura', os.arch());
 console.log(' CPUs', os.cpus()); //<---- vamos a poder escalar procesos en Node
-console.log(' Memoria libre', os.freemem() / 1024/1024);
-console.log('memoria Total', os.totalmem() / 1024/ 1024); 
+console.log(' Memoria libre', os.freemem() / 1024 / 1024);
+console.log('memoria Total', os.totalmem() / 1024 / 1024);
 console.log('uptime', os.uptime());
