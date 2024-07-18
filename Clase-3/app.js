1d94cc75d2f6a80dc66ac6ce7ab514ded8bd92d3
Simon <simontorbett@gmail.com>
2024-07-17 20:26:26 -0400

    clase3

--- Clase-3/app.js
+++ Clase-3/app.js
@@ -6,12 +6,24 @@ app.disable('x-powered-by');
 
 
 app.get('/movies', (req, res) => {
+    const {genre} = req.query
+if (genre) {
+    const filteredMovies = movies.filter(
+        movie => movie.genre.some(g => g.toLowerCase() == genre.toLowerCase())
+    )
+    return res.json(filteredMovies)
+}
     res.json(movies)
+
 })
 
 app.get('/movies/:id', (req, res) => {   // como acceder al id de todas formas... path-to-regexp <-- biblioteca
 
     const { id } = req.params
+    const movie = movies.find(movie => movie.id === id)
+    if (movie) return res.json(movie)
+    
+    res.status(404).json({message: 'movie not found'})
 
 })
 
