6f69ca6a5e721ba3da09e0cecbf103728be311ab
Simon <simontorbett@gmail.com>
2024-07-20 15:31:32 -0400

    Crear validaciones

--- Clase-3/schemas/movies.js
+++ Clase-3/schemas/movies.js
@@ -7,12 +7,13 @@ const movieSchema = z.object ({
     }),
     year: z.number().int().min(1900).max(2024),
     director: z.string(),
-    duration: z.number().min(0).max(10),
+    duration: z.number().min().max(),
+    rate: z.number().min(0).max(10).default(5.5),
     poster: z.string().url({
     message: 'Poster must be a valiD URL'
 }),
 genre: z.array(
-    z.enum(['Action','Adnventure','Comedy', 'Drama','Fantasy','Horror','Thriller','Sci-gi']),
+    z.enum(['Action','Adnventure','Comedy', 'Drama','Crime','Fantasy','Horror','Thriller','Sci-gi']),
 {
     required_error: 'movie genre is required',
     invalid_type_error: 'movie type must be an array of enum genre'
