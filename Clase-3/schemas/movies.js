bdb863709a330a1ac84862f8d5dd7b5e1b9f8f2e
Simon <simontorbett@gmail.com>
2024-07-20 16:52:50 -0400

    Error de cors reparado

--- Clase-3/schemas/movies.js
+++ Clase-3/schemas/movies.js
@@ -1,34 +1,35 @@
 const z = require('zod')
 
-const movieSchema = z.object ({
-    title: z.string({
-        invalid_type_error: ' Movie Title must be a string',
-        required_error:'Movie title is required'
-    }),
-    year: z.number().int().min(1900).max(2024),
-    director: z.string(),
-    duration: z.number().min().max(),
-    rate: z.number().min(0).max(10).default(5.5),
-    poster: z.string().url({
-    message: 'Poster must be a valiD URL'
-}),
-genre: z.array(
-    z.enum(['Action','Adnventure','Comedy', 'Drama','Crime','Fantasy','Horror','Thriller','Sci-gi']),
-{
-    required_error: 'movie genre is required',
-    invalid_type_error: 'movie type must be an array of enum genre'
-}
-)
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required.'
+  }),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url({
+    message: 'Poster must be a valid URL'
+  }),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Crime', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+    {
+      required_error: 'Movie genre is required.',
+      invalid_type_error: 'Movie genre must be an array of enum Genre'
+    }
+  )
 })
 
-function validateMovie (input){
-    return movieSchema.safeParse(input)
+function validateMovie (input) {
+  return movieSchema.safeParse(input)
 }
 
 function validatePartialMovie (input) {
-    return movieSchema.partial().safeParse(input)
+  return movieSchema.partial().safeParse(input)
 }
 
-module.exports = {validateMovie,
-    validatePartialMovie
+module.exports = {
+  validateMovie,
+  validatePartialMovie
 }
