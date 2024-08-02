84677d4aee574e786af46f35119c70ef5e1f27d0
Simon <simontorbett@gmail.com>
2024-08-02 19:34:12 -0400

    inyeccion de dependencias

--- Clase-5/controllers/movies.js
+++ Clase-5/controllers/movies.js
@@ -1,57 +1,59 @@
-// import { MovieModel } from "../models/local-file-sistem/movie.js"
-import { MovieModel } from "../models/mysql/movies.js"
-import {validateMovie, validatePartialMovie} from '../schemas/movies.js'
+import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
 
 export class MovieController {
-    static async getAll (req, res) {
-      const { genre } = req.query
-      const movies = await MovieModel.getAll({ genre })
-      res.json(movies)
-    }
-  
-    static async getById (req, res) {
-      const { id } = req.params
-      const movie = await MovieModel.getById({ id })
-      if (movie) return res.json(movie)
-      res.status(404).json({ message: 'Movie not found' })
-    }
-  
-    static async create (req, res) {
-      const result = validateMovie(req.body)
-  
-      if (!result.success) {
-      // 422 Unprocessable Entity
-        return res.status(400).json({ error: JSON.parse(result.error.message) })
-      }
-  
-      const newMovie = await MovieModel.create({ input: result.data })
-  
-      res.status(201).json(newMovie)
-    }
-  
-    static async delete (req, res) {
-      const { id } = req.params
-  
-      const result = await MovieModel.delete({ id })
-  
-      if (result === false) {
-        return res.status(404).json({ message: 'Movie not found' })
-      }
-  
-      return res.json({ message: 'Movie deleted' })
-    }
-  
-    static async update (req, res) {
-      const result = validatePartialMovie(req.body)
-  
-      if (!result.success) {
-        return res.status(400).json({ error: JSON.parse(result.error.message) })
-      }
-  
-      const { id } = req.params
-  
-      const updatedMovie = await MovieModel.update({ id, input: result.data })
-  
-      return res.json(updatedMovie)
-    }
+  constructor ({ movieModel }) {
+    this.movieModel = movieModel
   }
+
+  getAll = async (req, res) => {
+    const { genre } = req.query
+    const movies = await this.movieModel.getAll({ genre })
+    res.json(movies)
+  }
+
+  getById = async (req, res) => {
+    const { id } = req.params
+    const movie = await this.movieModel.getById({ id })
+    if (movie) return res.json(movie)
+    res.status(404).json({ message: 'Movie not found' })
+  }
+
+  create = async (req, res) => {
+    const result = validateMovie(req.body)
+
+    if (!result.success) {
+    // 422 Unprocessable Entity
+      return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const newMovie = await this.movieModel.create({ input: result.data })
+
+    res.status(201).json(newMovie)
+  }
+
+  delete = async (req, res) => {
+    const { id } = req.params
+
+    const result = await this.movieModel.delete({ id })
+
+    if (result === false) {
+      return res.status(404).json({ message: 'Movie not found' })
+    }
+
+    return res.json({ message: 'Movie deleted' })
+  }
+
+  update = async (req, res) => {
+    const result = validatePartialMovie(req.body)
+
+    if (!result.success) {
+      return res.status(400).json({ error: JSON.parse(result.error.message) })
+    }
+
+    const { id } = req.params
+
+    const updatedMovie = await this.movieModel.update({ id, input: result.data })
+
+    return res.json(updatedMovie)
+  }
+}
---
+++ Clase-5/server-with-local.js
@@ -0,0 +1,5 @@
+import { createApp } from './app.js'
+
+import { MovieModel } from './models/local-file-sistem/movie.js'
+
+createApp({ movieModel: MovieModel })
