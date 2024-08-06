bd91f4e62cf81e4a287972624b8c12dfed7bd28b
Simon <simontorbett@gmail.com>
2024-08-05 20:26:56 -0400

    DEPENDENCIAS

--- Clase-6/server/index.js
+++ Clase-6/server/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import logger from 'morgan'
 
 
 
 const port = process.env.PORT ?? 3000
 
 const app = express()
+app.use(logger('dev'))
 
 app.get('/', (req, res) => {
 res.send('<h1> Esto es el chat</h1>')
