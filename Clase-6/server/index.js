ca1117f80699d39b6be2294d7274351d0eb96d8e
Simon <simontorbett@gmail.com>
2024-08-06 20:51:29 -0400

    el cliente se comunica con el servidor

--- Clase-6/server/index.js
+++ Clase-6/server/index.js
@@ -16,6 +16,9 @@ io.on('connection',(socket) => {
     socket.on('disconnect', () => {
         console.log('a user disconnected!')
     })
+    socket.on('chat message',(msg) => {
+        console.log('message: ' + msg)
+    })
 })
 
 
